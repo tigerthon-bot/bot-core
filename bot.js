f323e9a4ee882bffa6eb4d02c6e57364146bbdf2
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2021-10-30 19:03:30 -0400

    set up s3

--- bot.js
+++ bot.js
@@ -1,11 +1,18 @@
 const puppeteer = require('puppeteer');
+const uploadFile = require('./uploadFile');
 
-(async () => {
+const run = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://google.com');
 
-  await page.screenshot({ path: 'example.png' });
+  const screenshot = await page.screenshot();
+
+  const uploadResult = await uploadFile(screenshot);
+
+  console.log(uploadResult);
 
   await browser.close();
-})();
+};
+
+run();
