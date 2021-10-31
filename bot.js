d552ec43118f185c9fcfb76bd9eb1c7942b623c3
GeoMmm <george@muammar.net>
2021-10-31 17:10:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,52 +17,63 @@ const url = require('url');
   const term = search_params.req.url("term")						// CONFIGURE parameter names 
   const pageurl = search_params.get("pageurl")
 */
-const term="sale";
+const term="tiger";
 var pageurl="https://animalskinsandbones.com/"
 
 // open page and search for term
   await page.goto(pageurl);  
   await page.type('input[name=s]', term);
   await page.keyboard.press('\n');
-  pageurl = await page.url();					// THIS IS NOT GIVING ME THE FULL URL INCLUDING SEARCH PARAMETERS
-  pageurl = "https://animalskinsandbones.com/?s=&post_type=product";		// CHEATING
+  pageurl = await page.url();
 
-// TODO: Manage no results found
-  
+
 // get number of results
+ /* (async() => {
+	try 
+	{
+		page.waitForSelector('p.woocommerce-result-count');
+	} catch(error)   { console.log("No results found for "+term) };		// TODO IF NO RESULTS FROM SEARCH
+  } )  */
+  
   await page.waitForSelector('p.woocommerce-result-count');
   let element = await page.$('p.woocommerce-result-count');
   let strNResults = await page.evaluate(el => el.textContent, element);
   let arrNResults = strNResults.split(" ");
-  const intMaxResults = eval(arrNResults[3]);
-  console.log("MaxResults = " + intMaxResults);
-  
-  let arrShowResults = (arrNResults[1]).split("–");
-  let intMinShowResults = eval(arrShowResults[0]);
-  let intMaxShowResults = eval(arrShowResults[1]);
+  console.log(arrNResults);
+  if (arrNResults[1] == "all")
+  {
+	  // only one page of results
+	  intMaxResults = eval(arrNResults[2]);
+	  intMinShowResults = 1;
+	  intMaxShowResults = intMaxResults;
+  }
+  else
+  {
+	  // multiple results pages
+	  const intMaxResults = eval(arrNResults[3]);
+	  console.log("MaxResults = " + intMaxResults);
+	  let arrShowResults = (arrNResults[1]).split("–");
+	  console.log(arrShowResults);
+	  intMinShowResults = eval(arrShowResults[0]);
+	  intMaxShowResults = eval(arrShowResults[1]);
+  }
   console.log("Showing " + intMinShowResults + " ... " + intMaxShowResults);
 
   const ProductsPerPage = 15;									// CONFIGURE Products per page. We find more than 15 links in each page although it counts 15 products. tbd
   var PageNumber = 1;
   var OldPageNumber = 1;
   
-  var StartResult = intMinShowResults;
+  let StartResult = intMinShowResults;
   
   do {
 
-	console.log(StartResult);
 	  // deal with supplementary pages in search results
-	  if (StartResult == 1)
-	  {
+	  if (StartResult = 1)
 		  pageurl=pageurl.replace("https://animalskinsandbones.com/","https://animalskinsandbones.com/page/1/");
-		  console.log(pageurl);
-	  }
-	  else 
-	  {
+	  else {
 		 OldPageNumber = PageNumber++;
          pageurl=pageurl.replace("/page/" + OldPageNumber + "/", "/page/" + PageNumber + "/") 
-	     await page.goto(pageurl);
-		 console.log(pageurl);
+	     await page1.goto();
 	  }
   
 	  // works: get all links in the page
@@ -72,6 +83,7 @@ var pageurl="https://animalskinsandbones.com/"
 	  return hrefs2.indexOf(elem) == pos;
 	  })
 	  console.log(hrefs);
+
 	  // iterate over the array of links 
 	  for (var link of hrefs)
 	  {
@@ -84,14 +96,14 @@ var pageurl="https://animalskinsandbones.com/"
 		 await page2.waitForTimeout(3000);
 		 var name = (link.replace("https://animalskinsandbones.com/product/"))
 		 name = name.replace("/","");
-		 console.log('Saving ' + name + '.png');				// SOMETHING WRONG WITH FILENAME. IT ADDS undefined IN FRONT OF NAME
-		 await page2.screenshot({ path: name + '.png' });                       // THIS IS WHERE IT SHOULD UPLOAD THE IMAGE
+		 console.log('Saving ' + name + '.png');			// TODO: SOMETHING WRONG WITH FILENAME. IT ADDS undefined IN FRONT OF NAME
+		 await page2.screenshot({ path: name + '.png' });	// THIS IS WHERE IT SHOULD UPLOAD THE IMAGE
 		 await page.waitForTimeout(2000);					// wait a couple of seconds just in case
 		 await page2.close 	 
-	  }	  
+	  }
+	  
 	// let's see if there are other pages for this search
 	StartResult = StartResult + ProductsPerPage;
-	console.log("StartResult" + StartResult)
   } while (StartResult < intMaxResults);					// condition to open next search results page
   
 	
