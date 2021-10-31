bdf3b567fbdc4d2df404023d22db27b99a13a2b6
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2021-10-31 13:01:39 -0400

    re set up cron

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 const job = require('./job');
 const uploadFile = require('./uploadFile');
 
-// const cron = new CronJob('0 0 1 * * *', async () => {
-job();
-// });
+const cron = new CronJob('0 0 1 * * *', async () => {
+  await job();
+});
 
-// cron.start();
+cron.start();
